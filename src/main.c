500ecfbe1e6a3eae54a0181b12c68b731b4f449b
Gryxon0x <fabiofabio101@o2.pl>
2026-06-08 14:57:34 +0200

    changed main for BLE debugging

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/sys/printk.h>
 
 #include "bma400_app.h"
+#include "ble_data_service.h"
 
 #define SAMPLE_PERIOD_MS 20
 
@@ -14,11 +15,8 @@ int main(void)
     k_msleep(3000);
 
     printk("\n\n=== BMA400 firmware boot ===\n");
-    printk("Before bma400_app_init()\n");
 
     ret = bma400_app_init();
-
-    printk("After bma400_app_init(), ret=%d\n", ret);
 
     if (ret != 0) {
         while (1) {
@@ -27,37 +25,46 @@ int main(void)
         }
     }
 
-    printk("sample_id,t_ms,ax_raw,ay_raw,az_raw,ax_mg,ay_mg,az_mg\n");
+    // printk("sample_id,t_ms,ax_raw,ay_raw,az_raw,ax_mg,ay_mg,az_mg\n");
 
-    t0_ms = k_uptime_get();
+    // t0_ms = k_uptime_get();
+
+    ret = ble_data_service_init();
+    printk("ble_data_service_init red=%d\n", ret);
 
     while (1) {
-        struct bma400_app_accel_raw accel;
-        int64_t t_ms = k_uptime_get() - t0_ms;
+        // struct bma400_app_accel_raw accel;
+        // int64_t t_ms = k_uptime_get() - t0_ms;
 
-        ret = bma400_app_read_accel_raw(&accel);
+        // ret = bma400_app_read_accel_raw(&accel);
 
-        if (ret == 0) {
-            int32_t ax_mg = bma400_app_raw_to_mg(accel.x);
-            int32_t ay_mg = bma400_app_raw_to_mg(accel.y);
-            int32_t az_mg = bma400_app_raw_to_mg(accel.z);
+        // if (ret == 0) {
+        //     int32_t ax_mg = bma400_app_raw_to_mg(accel.x);
+        //     int32_t ay_mg = bma400_app_raw_to_mg(accel.y);
+        //     int32_t az_mg = bma400_app_raw_to_mg(accel.z);
 
-            printk("%u,%lld,%d,%d,%d,%d,%d,%d\n",
-                   sample_id,
-                   t_ms,
-                   accel.x,
-                   accel.y,
-                   accel.z,
-                   (int)ax_mg,
-                   (int)ay_mg,
-                   (int)az_mg);
+        //     printk("%u,%lld,%d,%d,%d,%d,%d,%d\n",
+        //            sample_id,
+        //            t_ms,
+        //            accel.x,
+        //            accel.y,
+        //            accel.z,
+        //            (int)ax_mg,
+        //            (int)ay_mg,
+        //            (int)az_mg);
 
-            sample_id++;
-        } else {
-            printk("bma400_app_read_accel_raw failed: %d\n", ret);
-        }
+        //     sample_id++;
+        // } else {
+        //     printk("bma400_app_read_accel_raw failed: %d\n", ret);
+        // }
 
-        k_sleep(K_MSEC(SAMPLE_PERIOD_MS));
+        // k_sleep(K_MSEC(SAMPLE_PERIOD_MS));
+
+        printk("Heartbeat: connected=%d notify=%d\n",
+            ble_data_service_is_connected(),
+            ble_data_service_notifications_enabled());
+
+        k_sleep(K_SECONDS(5));
     }
 
     return 0;
