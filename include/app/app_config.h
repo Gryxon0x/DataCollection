efd97105876a6dd7aac29ccb4568be3f229e421b
Gryxon0x <fabiofabio101@o2.pl>
2026-08-12 10:06:08 +0200

    changes in config for multiple devices

---
+++ include/app/app_config.h
@@ -0,0 +1,12 @@
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+#ifndef APP_DEVICE_ID
+#define APP_DEVICE_ID 1
+#endif
+
+#ifndef APP_DEVICE_NAME
+#define APP_DEVICE_NAME "BMA400_WRIST"
+#endif
+
+#endif /* APP_CONFIG_H_ */
